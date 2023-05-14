37fd12708a9ed2d16cb8a83f92df0dc56e6a9ab0
00rest <119826790+00rest@users.noreply.github.com>
2023-05-14 11:14:44 -0400

    Models done

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, Types } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// Virtual property `reactionCount` that counts how many reactions this thought has
+userSchema.virtual('reactionCount').get(function () { return `${this.reactions.length}`});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+      max_length: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: () => Promise.resolve(false),
+        message: 'Email validation failed'
+      }
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thoughts',
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+// Virtual property `friendCount` that counts how many friends this looser has
+userSchema.virtual('friendCount').get(function () { return `${this.friends.length}`});
+
+
+const User = model('user', userSchema);
+
+module.exports = User;
